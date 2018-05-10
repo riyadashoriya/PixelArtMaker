633abd0a6ca41843e92aa7b0673a1573bf8cddf4
RIYA DASHORIYA <riyadashoriya@gmail.com>
2018-05-10 10:55:19 -0600

    D12 Create dynamic table for pixel art maker using the input provided by user

--- designs.js
+++ designs.js
@@ -1,8 +1,23 @@
 // Select color input
-// Select size input
+var color = document.getElementById('colorPicker').value;
 
 // When size is submitted by the user, call makeGrid()
+document.getElementById('submit').addEventListener("click", function(e){
+  e.preventDefault();
+  let height = document.getElementById('inputHeight').value;
+  let width = document.getElementById('inputWidth').value;
+  let table = document.getElementById('pixelCanvas');
+  table.innerHTML = "";
+  makeGrid(height, width, table);
+});
 
-function makeGrid() {
-  // Your code goes here!
+function makeGrid(height, width, table) {
+  for(var i = 0; i < height; i++){
+    let tr = document.createElement('tr');
+    for (var j = 0; j < width; j++){
+        let td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
 }

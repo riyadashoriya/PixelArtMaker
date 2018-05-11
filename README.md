fd5caed91968f239124eefea6d08867e6fea3af9
RIYA DASHORIYA <riyadashoriya@gmail.com>
2018-05-11 11:19:00 -0600

    D13 Dynamically change the color of table cell whenever new color is selected by the user

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # PixelArtMaker
 
-## Table of Contents
+Develop a one-page web application that lets users draw an amazing 8-bit pixel masterpiece image on a customized panel.
+You can:
+1. Dynamically set the size of the table as an N by M grid.
+2. Choose a color.
+3. Click a cell in the grid to fill that cell with the chosen color.
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-
-## Instructions
-
-To get started, open `designs.js` and start building out the app's functionality.
+Note: You must be using the current version of Edge, Firefox, or Chrome. Due to implementation differences, this exercise does not work effectively in Safari or Opera.
--- designs.js
+++ designs.js
@@ -1,21 +1,34 @@
-// Select color input
-var color = document.getElementById('colorPicker').value;
 
 // When size is submitted by the user, call makeGrid()
 document.getElementById('submit').addEventListener("click", function(e){
+  //Prevent refresh of page when form is submitted.
   e.preventDefault();
+  //Get the user input height and width.
   let height = document.getElementById('inputHeight').value;
   let width = document.getElementById('inputWidth').value;
   let table = document.getElementById('pixelCanvas');
+  //Clear the table elements, whenever new dimensions are entered.
   table.innerHTML = "";
+  //Create table
   makeGrid(height, width, table);
 });
 
+var color = document.getElementById('colorPicker');
+//Change the color whenever a new color is selected
+document.getElementById('colorPicker').addEventListener("change", function(){
+  color = colorPicker.value;
+});
+
+//Call this function to create a table when size of table is submitted
 function makeGrid(height, width, table) {
-  for(var i = 0; i < height; i++){
+  for(var i = 0; i < height; i++) {
     let tr = document.createElement('tr');
-    for (var j = 0; j < width; j++){
+    for (var j = 0; j < width; j++) {
         let td = document.createElement('td');
+        td.addEventListener("click", function() {
+          //Whenever any cell is clicked, change the color of the cell.
+          td.style.backgroundColor = color;
+        });
         tr.appendChild(td);
     }
     table.appendChild(tr);
